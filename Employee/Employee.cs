768def57337763e45705cc6dcc741ead5d53ab3f
MRDarsh <mrdarshh@users.noreply.github.com>
2022-03-30 20:26:39 +0200

    Add project files.

---
+++ Employee/Employee.cs
@@ -0,0 +1,127 @@
+﻿namespace Company
+{
+    using System;
+    class Company
+    {
+        public static void Main()
+        {
+            Console.WriteLine("how many Employee do you want");
+            Employee[] employees = new Employee[ask_for_int()];
+            for (int i = 0; i < employees.Length; i++)
+            {
+                employees[i] = new Employee();
+                Console.WriteLine("Enter employee name");
+                employees[i].name = ask_for_string();
+                Console.WriteLine("Enter employee age");
+                employees[i].age = ask_for_int();
+                Console.WriteLine("Enter employee id");
+                employees[i].id = ask_for_int();
+                employees[i].ChangeJob();
+                Console.WriteLine("Enter employee salary (for dufalt value enter \"-1\"");
+                employees[i].salary = ask_for_double();
+                Console.WriteLine("Enter employee working bonus Hours");
+                employees[i].workingbonushours = ask_for_double();
+            }
+            Console.WriteLine("____________________________________________");
+            foreach(var emp in employees)
+            {
+                Console.WriteLine($"name: {emp.name}");
+                Console.WriteLine($"age: {emp.age}");
+                Console.WriteLine($"id: {emp.id}");
+                Console.WriteLine($"departTybe: {emp.departmentName}");
+                Console.WriteLine($"salary: {emp.salary}$");
+                Console.WriteLine($"working bonus Hours: {emp.workingbonushours}");
+                Console.WriteLine($"bonus :{emp.bonus()}$");
+                Console.WriteLine("____________________________________________");
+            }
+
+
+            static char ask_for_char()
+            {
+                char ask_char;
+                do
+                {
+                    try
+                    {
+                        ask_char = char.Parse(Console.ReadLine());
+                        return ask_char;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Error, please enter correct symbol");
+                    }
+                } while (true);
+            }
+            static int ask_for_int()
+            {
+                int ask_number;
+                do
+                {
+                    try
+                    {
+                        ask_number = int.Parse(Console.ReadLine());
+                        return ask_number;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Error, Please enter a real number");
+                    }
+                } while (true);
+            }
+            static float ask_for_float()
+            {
+                float ask_number;
+                do
+                {
+                    try
+                    {
+                        ask_number = float.Parse(Console.ReadLine());
+                        return ask_number;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Error, Please enter a real number");
+                    }
+                } while (true);
+            }
+            static double ask_for_double()
+            {
+                float ask_number;
+                do
+                {
+                    try
+                    {
+                        ask_number = float.Parse(Console.ReadLine());
+                        return ask_number;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Error, Please enter a real number");
+                    }
+                } while (true);
+            }
+            static string ask_for_string()
+            {
+                string value;
+                do
+                {
+                    try
+                    {
+                        value = Console.ReadLine();
+                        if (string.IsNullOrEmpty(value))
+                        {
+                            Console.WriteLine("Error, Please enter the student name");
+                            continue;
+                        }
+                        else
+                            return value;
+                    }
+                    catch (Exception)
+                    {
+                        throw new("Invalid input value ");
+                    }
+                } while (true);
+            }
+        }
+    }
+}
---
+++ Employee/Employee.h
@@ -0,0 +1,151 @@
+﻿public class person
+{
+    string Name;
+    int Age;
+    int ID;
+
+    public string name
+    {
+        get { return Name; }
+        set { this.Name = value; }
+    }
+    public int age
+    { get { return Age; } set { this.Age = value; } }
+    public int id
+    { get { return ID; } set { this.ID = value; } }
+    public person()
+    {
+        Name = "";
+        Age = 0;
+        ID = 0;
+    }
+    public person(string name, int age, int id)
+    {
+        this.Name = name;
+        this.Age = age;
+        this.ID = id;
+    }
+}
+public class Employee : person
+{
+    double WorkingbonusHours;
+    string DepartmentName;
+    double Salary;
+    public Employee()
+    {
+        WorkingbonusHours = 0;
+        DepartmentName = "No Titel";
+        Salary = 0;
+    }
+    public double workingbonushours
+    {
+        get { return this.WorkingbonusHours; }
+        set { WorkingbonusHours = value; }
+    }
+
+
+    struct jobsSalary
+    {
+        public const double Accountant = 4000;
+        public const double Manager = 6000;
+        public const double Technician = 5000;
+        public const double FirstClassWorker = 2000;
+        public const double SecondClassWorker = 3000;
+        public const double Trainee = 1050.50;
+        public const double NoTitel = 0;
+    }
+    public double salary
+    {
+        set
+        {
+            Salary = value;
+        }
+        get
+        {
+            switch (DepartmentName)
+            {
+                case "Accountant": return jobsSalary.Accountant;
+                case "Manager": return jobsSalary.Manager;
+                case "Technician": return jobsSalary.Technician;
+                case "First-class worker": return jobsSalary.FirstClassWorker;
+                case "Second-class worker": return jobsSalary.SecondClassWorker;
+                case "Trainee": return jobsSalary.Trainee;
+                case "No Titel": return jobsSalary.NoTitel;
+                default:
+                    throw new Exception("wrong entry reason (job title not exist)");
+            }
+        }
+    }
+    ////Accountant, technician, manager, trainee, second-class worker, first-class worker
+    public string departmentName
+    {
+        get { return DepartmentName; }
+        set
+        {
+            DepartmentName = value;
+            //switch (value.ToLower())
+            //{
+            //    case "1":
+            //    case "accountant": departmentName = "Accountant"; break;
+            //    case "2":
+            //    case "mechnician": departmentName = "Technician"; break;
+            //    case "3":
+            //    case "manager": departmentName = "Manager"; break;
+            //    case "4":
+            //    case "first-class worker": departmentName = "First-class worker"; break;
+            //    case "5":
+            //    case "second-class worker": departmentName = "Second-class worker"; break;
+            //    case "6":
+            //    case "trainee": departmentName = "Trainee"; break;
+            //    case "7":
+            //    case "no titel": departmentName = "No Titel"; break;
+            //    default:
+            //        throw new Exception("wrong entry reason (job number or title not exist)");
+            //}
+        }
+    }
+    public void ChangeJob()
+    {
+        Console.WriteLine("Enter Employee job (Enter job number or name title)");
+        Console.WriteLine("1- Accountant\t2-technician\t3-manager");
+        Console.WriteLine("4- first-class worker\t5-second-class worker\t6-trainee\t7-no titel");
+        string NewJob = Console.ReadLine();
+        switch (NewJob.ToLower())
+        {
+            case "1":
+            case "accountant": departmentName = "Accountant"; break;
+            case "2":
+            case "mechnician": departmentName = "Technician"; break;
+            case "3":
+            case "manager": departmentName = "Manager"; break;
+            case "4":
+            case "first-class worker": departmentName = "First-class worker"; break;
+            case "5":
+            case "second-class worker": departmentName = "Second-class worker"; break;
+            case "6":
+            case "trainee": departmentName = "Trainee"; break;
+            case "7":
+            case "no titel": departmentName = "No Titel"; break;
+            default:
+                throw new Exception("wrong entry reason (job number or title not exist)");
+        }
+    }
+    public double bonus()
+    {
+        return workingbonushours *= (0.005 * salary);
+    }
+    //public Employee(string name = null, int age = 0, int id = -1, string departTybe = null, float workingHours = 0)
+    //{
+    //    this.name = name;
+    //    this.age = age;
+    //    this.id = id;
+    //    this.DepartTybe.departmentName = departTybe;
+    //    this.WorkingbonusHours = workingHours;
+    //}
+    public void ShowEmployee()
+    {
+        Console.WriteLine($"name: {name}\tage: {age}\tid: {id}");
+        Console.WriteLine($"Department: {departmentName}\tsalary: {salary}\t");
+    }
+}
+
